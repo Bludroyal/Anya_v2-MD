57a59b257332f5da490a33b114f9ffff709be0e4
Bludroyal <Bludroyal1@gmail.com>
2024-11-08 03:12:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,21 +16,21 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "☯︎shi ne✌︎💨",
+    ownernumber: process.env.Owner_Number || "2349164718113",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
      * other details and parameters
      */
     author: process.env.Author || "𝐏𝐢𝐤𝐚𝐁𝐨𝐭𝐳 🌚🌙",
-    packname: process.env.PackName || "𝕼𝖚𝖊𝖊𝖓 𝕬𝖓𝖞𝖆 𝖁2 𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝕻𝖆𝖈𝖐 🪀",
+    packname: process.env.PackName || "☯︎shi ne✌︎💨",
     socialLink: process.env.Web || "https://github.com/PikaBotz",
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "wkFCkHXVQQ2_A_N_Y_A_V_2_ZlecnBhAK",
 
     /**
      * thumbnails and medias
